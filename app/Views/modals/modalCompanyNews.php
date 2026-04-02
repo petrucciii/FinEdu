68a8c89c90e81230bb493ffa0599b2393dc5ab6c
petrucciii <riccardo.petrucci99@gmail.com>
2026-04-02 09:56:36 +0200

    feat(news): implemented modals for news management and views

---
+++ app/Views/modals/modalCompanyNews.php
@@ -0,0 +1,15 @@
+<div class="modal fade" id="companyNewsModal" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title"></h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <h6 class="text-muted" data-news-field="subtitle"></h6>
+                <p class="mt-3 mb-3" style="white-space: pre-wrap;" data-news-field="body"></p>
+                <small class="text-muted" data-news-field="meta"></small>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ app/Views/modals/modalNewsDelete.php
@@ -0,0 +1,20 @@
+<div class="modal fade" id="modalEliminaNews" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Archivia news</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form action="/admin/NewsManagementController/delete" method="post" id="formDeleteNews">
+                <div class="modal-body">
+                    <input type="hidden" name="news_id" id="delete_news_id" value="">
+                    <p class="mb-0">Confermi l'archiviazione di questa notizia? Non sarà più visibile sul sito.</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+                    <button type="submit" class="btn btn-danger">Archivia</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
